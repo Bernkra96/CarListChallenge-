6b833fdefb5cedfbe1ab605bc09da3594911722b
bernkra96 <bernkra@gmail.com>
2022-08-16 00:46:28 +0200

    Basic list coding works
    Only optical update and some text Needs to be completed.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -15,26 +15,56 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
+        ZStack {
             
             
             List {
-                ForEach(cars, id: \.model)
-                
-                { cars in
-                    
-                    Text (cars.make)
-                    
+                ForEach(cars, id: \.model) { cars in
+                    VStack {
+                        HStack {
+                            Image ("\(cars.model)")
+                                .resizable(resizingMode: .stretch)
+                                .aspectRatio(contentMode: .fit)
+                                .accessibilityLabel(" Image of \(cars.make)   \(cars.model)")
+                            
+                            Text ("\(cars.make)    \(cars.model) ")
+                        }
+                        
+                        
+                        HStack {
+                            
+                            Text ("\(cars.customerPrice)")
+                            
+                            Text ("\(cars.marketPrice)")
+                            
+                        }
+                        
+                        VStack {
+                            
+                            let cons = cars.consList.joined(separator: " " )
+                            
+                            let pros = cars.prosList.joined(separator: " " )
+                            
+                            Text (String ("\(pros)"))
+                            
+                            
+                            Text (String ("\(cons)"))
+                            
+                            
+                        }
+                        
+                        Text ("\(cars.rating)")
+                    }.padding(.all)
                 }
             }
         }
     }
-    
 }
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
