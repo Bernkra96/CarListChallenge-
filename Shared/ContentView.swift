3c190812ffd3b73324cf4e6ffc3d2de5a9b0cd6f
bernkra96 <bernkra@gmail.com>
2022-08-16 22:37:27 +0200

    Swift UI is now in a god useable state

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -15,46 +15,137 @@ struct ContentView: View {
     
     var body: some View {
         
-        ZStack {
-            
+        
+        
+        VStack {
+            Text ("Car List ")
+                .font(.title)
+                .fontWeight(.bold)
             
             List {
                 ForEach(cars, id: \.model) { cars in
-                    VStack {
+                    VStack (spacing: 8) {
+                        
+                        
+                        
                         HStack {
                             Image ("\(cars.model)")
                                 .resizable(resizingMode: .stretch)
                                 .aspectRatio(contentMode: .fit)
                                 .accessibilityLabel(" Image of \(cars.make)   \(cars.model)")
+                                .cornerRadius(20)
                             
                             Text ("\(cars.make)    \(cars.model) ")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .multilineTextAlignment(.center)
                         }
                         
                         
-                        HStack {
+                        VStack() {
                             
-                            Text ("\(cars.customerPrice)")
+                            Text (" Customer Price.: \(cars.customerPrice) $ ")
+                                .font(.body)
+                                .fontWeight(.semibold)
+                                .multilineTextAlignment(.center)
                             
-                            Text ("\(cars.marketPrice)")
+                            
+                            Text (" Market Price.: \(cars.marketPrice) $ ")
+                                .fontWeight(.semibold)
+                                .multilineTextAlignment(.center)
+                                .font(.body)
+                        }
+                        
+                        
+                        VStack (alignment: .center, spacing: 4.0) {
+                            let cons = cars.consList.joined(separator: " " )
+                            let pros = cars.prosList.joined(separator: " " )
+                            
+                            HStack {
+                                Text("Car Pros")
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                
+                                
+                                Image(systemName: "hand.thumbsup.fill")
+                                    .renderingMode(.original)
+                                    .foregroundColor(.orange)
+                                    .frame(width: 25.0, height: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                
+                                
+                                
+                            }
+                            Text (String ("\(pros)"))
+                                .font(.body)
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
+                                .lineLimit(14)
+                            
+                            
+                            
+                            HStack() {
+                                Text("Car Cons")
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                
+                                Image(systemName: "hand.thumbsdown.fill")
+                                    .renderingMode(.original)
+                                    .foregroundColor(.orange)
+                                    .frame(width: 25.0, height: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                
+                                
+                            }
+                            Text (String ("\(cons)"))
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
+                                .lineLimit(12)
+                                .font(.body)
+                            
                             
                         }
+                        .frame(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/185.0/*@END_MENU_TOKEN@*/)
+                        
                         
                         VStack {
                             
-                            let cons = cars.consList.joined(separator: " " )
-                            
-                            let pros = cars.prosList.joined(separator: " " )
-                            
-                            Text (String ("\(pros)"))
-                            
-                            
-                            Text (String ("\(cons)"))
-                            
-                            
+                            HStack {
+                                
+                                Image(systemName: cars.rating < 1 ? "star" :  "star.fill" )
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(cars.rating < 1 ? .gray  : .orange )
+                                    .frame(width: 40.0)
+                                Image(systemName: cars.rating < 2 ? "star" :  "star.fill" )
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(cars.rating < 2 ? .gray  : .orange )
+                                    .frame(width: 40.0)
+                                
+                                Image(systemName: cars.rating < 3 ? "star" :  "star.fill" )
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(cars.rating < 3 ? .gray  : .orange )
+                                    .frame(width: 40.0)
+                                
+                                
+                                Image(systemName: cars.rating < 4 ? "star" :  "star.fill" )
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(cars.rating < 4 ? .gray  : .orange )
+                                    .frame(width: 40.0)
+                                
+                                
+                                Image(systemName: cars.rating < 5 ? "star" :  "star.fill" )
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(cars.rating < 5 ? .gray  : .orange )
+                                    .frame(width: 40.0)
+                                
+                                
+                            }
+                            Text ("\(cars.rating) 0f 5")
                         }
-                        
-                        Text ("\(cars.rating)")
-                    }.padding(.all)
+                    }
                 }
             }
         }
