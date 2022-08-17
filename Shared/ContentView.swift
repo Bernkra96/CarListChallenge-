60018f9d2dcefad05dfb569e9bf32c4572d28a9f
bernkra96 <bernkra@gmail.com>
2022-08-17 21:20:14 +0200

    List and Navigation works
    DoTo : ad data
               ad app ui

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 
-
 struct ContentView: View {
     
     private var cars: [Car]  = Car.allCars
     
     var body: some View {
         
-        
+        NavigationView {
         
         VStack {
             Text ("Car List ")
@@ -24,6 +23,9 @@ struct ContentView: View {
             
             List {
                 ForEach(cars, id: \.model) { cars in
+                  
+                    NavigationLink( destination: DetailView(car: cars)) {
+                    
                     VStack (spacing: 8) {
                         
                         
@@ -40,122 +42,128 @@ struct ContentView: View {
                                 .fontWeight(.bold)
                                 .multilineTextAlignment(.center)
                         }
-                        
-                        
-                        VStack() {
-                            
-                            Text (" Customer Price.: \(cars.customerPrice) $ ")
-                                .font(.body)
-                                .fontWeight(.semibold)
-                                .multilineTextAlignment(.center)
-                            
-                            
-                            Text (" Market Price.: \(cars.marketPrice) $ ")
-                                .fontWeight(.semibold)
-                                .multilineTextAlignment(.center)
-                                .font(.body)
-                        }
-                        
-                        
-                        VStack (alignment: .center, spacing: 4.0) {
-                            let cons = cars.consList.joined(separator: " " )
-                            let pros = cars.prosList.joined(separator: " " )
-                            
-                            HStack {
-                                Text("Car Pros")
-                                    .font(.title2)
-                                    .fontWeight(.bold)
-                                
-                                
-                                Image(systemName: "hand.thumbsup.fill")
-                                    .renderingMode(.original)
-                                    .foregroundColor(.orange)
-                                    .frame(width: 25.0, height: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                                
-                                
-                                
-                            }
-                            Text (String ("\(pros)"))
-                                .font(.body)
-                                .fontWeight(.medium)
-                                .multilineTextAlignment(.center)
-                                .lineLimit(14)
-                            
-                            
-                            
-                            HStack() {
-                                Text("Car Cons")
-                                    .font(.title2)
-                                    .fontWeight(.bold)
-                                
-                                Image(systemName: "hand.thumbsdown.fill")
-                                    .renderingMode(.original)
-                                    .foregroundColor(.orange)
-                                    .frame(width: 25.0, height: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                                
-                                
-                            }
-                            Text (String ("\(cons)"))
-                                .fontWeight(.medium)
-                                .multilineTextAlignment(.center)
-                                .lineLimit(12)
-                                .font(.body)
-                            
-                            
-                        }
-                        .frame(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/185.0/*@END_MENU_TOKEN@*/)
-                        
-                        
-                        VStack {
-                            
-                            HStack {
-                                
-                                Image(systemName: cars.rating < 1 ? "star" :  "star.fill" )
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(cars.rating < 1 ? .gray  : .orange )
-                                    .frame(width: 40.0)
-                                Image(systemName: cars.rating < 2 ? "star" :  "star.fill" )
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(cars.rating < 2 ? .gray  : .orange )
-                                    .frame(width: 40.0)
-                                
-                                Image(systemName: cars.rating < 3 ? "star" :  "star.fill" )
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(cars.rating < 3 ? .gray  : .orange )
-                                    .frame(width: 40.0)
-                                
-                                
-                                Image(systemName: cars.rating < 4 ? "star" :  "star.fill" )
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(cars.rating < 4 ? .gray  : .orange )
-                                    .frame(width: 40.0)
-                                
-                                
-                                Image(systemName: cars.rating < 5 ? "star" :  "star.fill" )
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(cars.rating < 5 ? .gray  : .orange )
-                                    .frame(width: 40.0)
-                                
-                                
-                            }
-                            Text ("\(cars.rating) 0f 5")
-                        }
                     }
+                        //
+                        //                        VStack() {
+                        //
+                        //                            Text (" Customer Price.: \(cars.customerPrice) $ ")
+                        //                                .font(.body)
+                        //                                .fontWeight(.semibold)
+                        //                                .multilineTextAlignment(.center)
+                        //
+                        //
+                        //                            Text (" Market Price.: \(cars.marketPrice) $ ")
+                        //                                .fontWeight(.semibold)
+                        //                                .multilineTextAlignment(.center)
+                        //                                .font(.body)
+                        //                        }
+                        //
+                        //
+                        //                        VStack (alignment: .center, spacing: 4.0) {
+                        //                            let cons = cars.consList.joined(separator: " " )
+                        //                            let pros = cars.prosList.joined(separator: " " )
+                        //
+                        //                            HStack {
+                        //                                Text("Car Pros")
+                        //                                    .font(.title2)
+                        //                                    .fontWeight(.bold)
+                        //
+                        //
+                        //                                Image(systemName: "hand.thumbsup.fill")
+                        //                                    .renderingMode(.original)
+                        //                                    .foregroundColor(.orange)
+                        //                                    .frame(width: 25.0, height: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                        //
+                        //
+                        //
+                        //                            }
+                        //                            Text (String ("\(pros)"))
+                        //                                .font(.body)
+                        //                                .fontWeight(.medium)
+                        //                                .multilineTextAlignment(.center)
+                        //                                .lineLimit(14)
+                        //
+                        //
+                        //
+                        //                            HStack() {
+                        //                                Text("Car Cons")
+                        //                                    .font(.title2)
+                        //                                    .fontWeight(.bold)
+                        //
+                        //                                Image(systemName: "hand.thumbsdown.fill")
+                        //                                    .renderingMode(.original)
+                        //                                    .foregroundColor(.orange)
+                        //                                    .frame(width: 25.0, height: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                        //
+                        //
+                        //                            }
+                        //                            Text (String ("\(cons)"))
+                        //                                .fontWeight(.medium)
+                        //                                .multilineTextAlignment(.center)
+                        //                                .lineLimit(12)
+                        //                                .font(.body)
+                        //
+                        //
+                        //                        }
+                        //                        .frame(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/185.0/*@END_MENU_TOKEN@*/)
+                        //
+                        //
+                        //                        VStack {
+                        //
+                        //                            HStack {
+                        //
+                        //                                Image(systemName: cars.rating < 1 ? "star" :  "star.fill" )
+                        //                                    .resizable()
+                        //                                    .aspectRatio(contentMode: .fit)
+                        //                                    .foregroundColor(cars.rating < 1 ? .gray  : .orange )
+                        //                                    .frame(width: 40.0)
+                        //                                Image(systemName: cars.rating < 2 ? "star" :  "star.fill" )
+                        //                                    .resizable()
+                        //                                    .aspectRatio(contentMode: .fit)
+                        //                                    .foregroundColor(cars.rating < 2 ? .gray  : .orange )
+                        //                                    .frame(width: 40.0)
+                        //
+                        //                                Image(systemName: cars.rating < 3 ? "star" :  "star.fill" )
+                        //                                    .resizable()
+                        //                                    .aspectRatio(contentMode: .fit)
+                        //                                    .foregroundColor(cars.rating < 3 ? .gray  : .orange )
+                        //                                    .frame(width: 40.0)
+                        //
+                        //
+                        //                                Image(systemName: cars.rating < 4 ? "star" :  "star.fill" )
+                        //                                    .resizable()
+                        //                                    .aspectRatio(contentMode: .fit)
+                        //                                    .foregroundColor(cars.rating < 4 ? .gray  : .orange )
+                        //                                    .frame(width: 40.0)
+                        //
+                        //
+                        //                                Image(systemName: cars.rating < 5 ? "star" :  "star.fill" )
+                        //                                    .resizable()
+                        //                                    .aspectRatio(contentMode: .fit)
+                        //                                    .foregroundColor(cars.rating < 5 ? .gray  : .orange )
+                        //                                    .frame(width: 40.0)
+                        //
+                        
+                    }
+//                    Text ("\(cars.rating) 0f 5")
                 }
             }
         }
     }
 }
+}
+    
+    //    }
+//}
 
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.light)
+            
+        }
     }
 }
---
+++ Shared/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  Car List Challenge CwC
+//
+//  Created by Bernhard Kratky on 17.08.22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var car: Car
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(car: Car.CarTest)
+    }
+}
